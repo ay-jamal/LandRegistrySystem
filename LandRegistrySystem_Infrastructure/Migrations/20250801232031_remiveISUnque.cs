3c6606cd07bda2e685545e83dbd49e0a5f1c5535
Ayhem jamal <aylibyakiller@gmail.com>
2025-08-02 16:16:51 +0200

    to hassan

---
+++ LandRegistrySystem_Infrastructure/Migrations/20250801232031_remiveISUnque.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LandRegistrySystem_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class remiveISUnque : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Farms_FarmNumber",
+                table: "Farms");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmNumber",
+                table: "Farms",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FarmNumber",
+                table: "Farms",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Farms_FarmNumber",
+                table: "Farms",
+                column: "FarmNumber",
+                unique: true,
+                filter: "[FarmNumber] IS NOT NULL");
+        }
+    }
+}
